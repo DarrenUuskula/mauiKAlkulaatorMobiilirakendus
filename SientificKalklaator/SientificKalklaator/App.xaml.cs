5e75211737c899e3daad36cb5ddb87b6f8b41c55
DarrenUuskula <darren.uus@gmail.com>
2023-03-08 14:34:00 +0200

    CalculatorPage

--- SientificKalklaator/SientificKalklaator/App.xaml.cs
+++ SientificKalklaator/SientificKalklaator/App.xaml.cs
@@ -1,11 +1,35 @@
-﻿namespace SientificKalklaator;
+﻿#if WINDOWS
+using Microsoft.UI;
+using.Microsoft.UI.Windowing;
+using.Microsoft.Graphics;
+#endif
+
+
+namespace SientificKalklaator;
 
 public partial class App : Application
 {
+	const int WindowWidth = 540;
+	const int WindowHeight = 1000;
+
+
 	public App()
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (Handler, view) =>
+		{
+#if WINDOWS
+			var mauiWindow = Handler.VirtualView;
+			var nativeWindow = Handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+#endif
+
+		});
+		MainPage = new CalculatorPage();
 	}
 }
---
+++ SientificKalklaator/SientificKalklaator/Views/CalculatorPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace SientificKalklaator.Views;
+
+public partial class CalculatorPage : ContentPage
+{
+	public CalculatorPage()
+	{
+		InitializeComponent();
+	}
+}
